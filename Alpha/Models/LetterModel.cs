963e30406771f24f7646cf672c930db2cb087f60
AlexanderVolsky <geceron9@mail.ru>
2024-09-03 14:30:00 +0700

    LetterModel class

--- Alpha/Models/LetterModel.cs
+++ Alpha/Models/LetterModel.cs
@@ -1,12 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
-namespace Alpha.Models
+public class LetterModel : INotifyPropertyChanged
 {
-    class LetterModel
+    private string _symbol;
+
+    public required string Symbol
     {
+        get => _symbol;
+        set
+        {
+            // !null || Empty
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Symbol cannot be null or empty.");
+            }
+
+            // Alhabet type check
+            if (IsLatinLetter(value))
+            {
+                _symbol = value.ToUpper();
+            }
+            else if (IsCyrillicLetter(value))
+            {
+                _symbol = value.ToUpper();
+            }
+            else if (IsThaiLetter(value))
+            {
+                _symbol = value;
+            }
+            else
+            {
+                throw new ArgumentException("Unsupported alphabet symbol.");
+            }
+
+            OnPropertyChanged();
+        }
     }
+
+    public bool IsKnown { get; set; } = false;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    #region Alphabets
+
+    // Latin
+    private bool IsLatinLetter(string value)
+    {
+        return Regex.IsMatch(value, @"^[A-Za-z]$");
+    }
+
+    // Cyrillic
+    private bool IsCyrillicLetter(string value)
+    {
+        return Regex.IsMatch(value, @"^[А-Яа-яЁё]$");
+    }
+
+    // Thai
+    private bool IsThaiLetter(string value)
+    {
+        return Regex.IsMatch(value, @"^[ก-๙]$");
+    }
+
+    #endregion
 }
